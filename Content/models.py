f881f941487e368b8e81e8013931de5ae50010b0
Mahdi Namaki <mavenium@gmail.com>
2021-01-09 09:14:28 +0330

    add Blog model

--- Content/models.py
+++ Content/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+from ckeditor_uploader.fields import RichTextUploadingField
+
+
+class Blog(models.Model):
+    title = models.CharField(
+        max_length=256,
+        verbose_name=_('Title :'),
+        unique=True,
+        null=False,
+        blank=False
+    )
+    slug = models.SlugField(
+        max_length=256,
+        verbose_name=_('Slug :'),
+        unique=True,
+        null=False,
+        blank=False
+    )
+    thumbnail = models.ImageField(
+        upload_to='content/blog/thumbnail/',
+        verbose_name=_('Thumbnail :')
+    )
+    publish = models.BooleanField(
+        verbose_name=_('Publish :'),
+        default=True,
+        help_text=_('Will this post be published?')
+    )
+    content = RichTextUploadingField()
+
+    class Meta:
+        verbose_name = _('Blog')
+        verbose_name_plural = _('Blogs')
+
+    def __str__(self):
+        return self.title
