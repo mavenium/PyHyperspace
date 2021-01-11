883ed5e03d2b09abed43bc041b8d7e02743d6678
Mahdi Namaki <mavenium@gmail.com>
2021-01-11 09:10:34 +0330

    add BlogAdmin class

--- Content/admin.py
+++ Content/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
-# Register your models here.
+from Content import models
+
+
+class BlogAdmin(admin.ModelAdmin):
+    list_display = [
+        'title',
+        'publish',
+        'create_time',
+        'last_update_time'
+    ]
+    list_editable = (
+        'publish',
+    )
+    list_filter = (
+        'publish',
+        'create_time',
+        'last_update_time'
+    )
+    search_fields = (
+        'title',
+    )
+    prepopulated_fields = {
+        'slug': ('title',)
+    }
+
+
+admin.site.register(models.Blog, BlogAdmin)
