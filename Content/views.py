161054dd1eedbd94b8aa1cae2ba4cb13954c7c83
Mahdi Namaki <mavenium@gmail.com>
2021-01-27 23:24:34 +0330

    fix create_contact method

--- Content/views.py
+++ Content/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import render, redirect
 from django.views import View, generic
 
 from . import models
@@ -24,4 +25,26 @@ class BlogSingle(generic.DetailView):
 
 
 def create_contact(request):
-    pass
+    if request.is_ajax():
+        name = request.POST.get('name', None)
+        email = request.POST.get('email', None)
+        message = request.POST.get('message', None)
+        if name is not None and email is not None and message is not None:
+
+            models.ContactUs.objects.create(
+                name=name,
+                email=email,
+                message=message
+            )
+
+            data = {
+                'success': 'Your message has been saved successfully.'
+            }
+
+        else:
+            data = {
+                'error': 'Error inserting message!'
+            }
+        return JsonResponse(data)
+    else:
+        return redirect('Contact:index')
