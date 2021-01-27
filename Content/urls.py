6cc3a3ef38a8209905ee7b594123af1dbb9d27e0
Mahdi Namaki <mavenium@gmail.com>
2021-01-27 23:20:16 +0330

    add create_contact url

--- Content/urls.py
+++ Content/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('blog/<str:slug>/', views.BlogSingle.as_view(), name='blog_single'),
+    path('create_contact', views.create_contact, name='create_contact')
 ]
